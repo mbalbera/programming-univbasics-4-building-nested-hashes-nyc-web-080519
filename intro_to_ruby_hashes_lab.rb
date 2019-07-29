17c27d40bb2cdbaf62afdaf75996a3c22f8a74d7
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 15:40:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ def monopoly_with_third_tier
 	  names: {
 	    reading_railroad:{},
 	    pennsylvania_railroad:{},
-	    b_and_o_railroad:{}
+	    b_and_o_railroad:{},
+	    shortline_railroad:{}
 	  },
 	
 	  rent_in_dollars:
