b541f2d44cfd445e8c43f420dd8e27ae91ebc516
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 15:33:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 def monopoly_with_third_tier
   monopoly = Hash.new()
 	monopoly[:railroads][:pieces] = 4 
-	monopoly[:railroads][:names] = Hash.new()
+	monopoly[:railroads][:names] = {}
 	monopoly
 
 end
